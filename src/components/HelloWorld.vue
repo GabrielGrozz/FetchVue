c34cd0bcc3e53691d03baeb07169a1ac2d3713e7
Gabriel Luis <gabrielmemes268825@gmail.com>
2023-01-18 11:42:55 -0300

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Editsadasd
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
